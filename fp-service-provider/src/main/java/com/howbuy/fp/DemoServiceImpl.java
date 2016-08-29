a3f24a39d6e6b39a1bb7cccb4427a8902f0821de
haohan007008 <web9924@163.com>
2016-08-29 19:10:13 +0800

    some

--- fp-service-provider/src/main/java/com/howbuy/fp/DemoServiceImpl.java
+++ fp-service-provider/src/main/java/com/howbuy/fp/DemoServiceImpl.java
@@ -3,6 +3,7 @@ package com.howbuy.fp;
 public class DemoServiceImpl implements DemoService {
 
 	public String sayHello(String name) {
+		System.out.println(Thread.currentThread().getName() + "executing....");
 		return "Hello Dubbo,Hello " + name;  
 	}
 
